135433f8a2401bfca81d9c54b6f2f09bb24b9906
Julian-Samuel Gebühr <julian-samuel@gebuehr.net>
2022-01-14 23:42:33 +0100

    Add delete commands

--- bot.py
+++ bot.py
@@ -25,16 +25,36 @@ async def list_token(room, message):
     match = botlib.MessageMatch(room, message, bot, PREFIX)
 
     if match.is_not_from_this_bot() and match.prefix() and match.command("list"):
-        token_list = api.list_token()
+        token_list = api.list_tokens()
         await bot.api.send_text_message(room.room_id, f"{token_list}")
 
 @bot.listener.on_message_event
-async def list_token(room, message):
+async def create_token(room, message):
     match = botlib.MessageMatch(room, message, bot, PREFIX)
 
     if match.is_not_from_this_bot() and match.prefix() and match.command("create"):
         token_list = api.create_token()
         await bot.api.send_text_message(room.room_id, f"{token_list}")
+
+@bot.listener.on_message_event
+async def delete_all_token(room, message):
+    match = botlib.MessageMatch(room, message, bot, PREFIX)
+
+    if match.is_not_from_this_bot() and match.prefix() and match.command("delete-all"):
+        token_list = api.delete_all_token()
+        await bot.api.send_text_message(room.room_id, f"Deleted the following token {token_list}")
+
+@bot.listener.on_message_event
+async def delete_token(room, message):
+    match = botlib.MessageMatch(room, message, bot, PREFIX)
+
+    if match.is_not_from_this_bot() and match.prefix() and match.command("delete"):
+        deleted_tokens = []
+        for token in match.args():
+            api.delete_token(token)
+            deleted_tokens.append(token)
+        await bot.api.send_text_message(room.room_id, f"Deleted the following token {deleted_tokens}")
+
 
 bot.run()
 
--- registration_api.py
+++ registration_api.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 
 
 class RegistrationAPI():
-    def __init__(self, base_url:str, token:str):
+    def __init__(self, base_url: str, token: str):
         self.base_url = base_url
         self.token = token
         self.headers = CaseInsensitiveDict()
@@ -13,19 +13,38 @@ class RegistrationAPI():
     def __str__(self):
         return f"API Connection to {self.base_url}"
 
-    def list_token(self):
+    def list_tokens(self):
+        """
+        Gathers a list of all registration tokens
+
+        :return: List of token
+        """
         r = requests.get(self.base_url, headers=self.headers)
-        return r.json()
+        return r.json()["registration_tokens"]
+
+    def delete_all_token(self):
+        """
+        Deletes all token
+
+        :return: List of deleted token
+        """
+        all_tokens = self.list_tokens()
+        for token in all_tokens:
+            self.delete_token(token["token"])
+        return all_tokens
+
+    def delete_token(self, token: str):
+        r = requests.delete(self.base_url + f"/{token}", headers=self.headers)
+        print(r)
 
     def create_token(self, expiry_days=7):
         """
         Create a token for registering an user
 
         expire_days:int
-            Determins how long the token will be valid (in days)
+            Determines how long the token will be valid (in days)
         """
-        expiry_time = int(datetime.timestamp(datetime.now() +  timedelta(days=expiry_days))*1000)
-        data = '{"uses_allowed": 1, "expiry_time": '+str(expiry_time)+'}'
-        r = requests.post(self.base_url+"/new", headers=self.headers, data=data)
+        expiry_time = int(datetime.timestamp(datetime.now() + timedelta(days=expiry_days)) * 1000)
+        data = '{"uses_allowed": 1, "expiry_time": ' + str(expiry_time) + '}'
+        r = requests.post(self.base_url + "/new", headers=self.headers, data=data)
         return r.json()
-
