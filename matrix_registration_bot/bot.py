1e594340e23fd719ec80dca66a7504e701fe8423
Julian-Samuel Gebühr <julian-samuel@gebuehr.net>
2022-02-03 14:05:53 +0100

    Add logging and password based login

--- matrix_registration_bot/bot.py
+++ matrix_registration_bot/bot.py
@@ -2,27 +2,44 @@ import simplematrixbotlib as botlib
 import matrix_registration_bot
 from matrix_registration_bot.registration_api import RegistrationAPI
 import yaml
+import logging
 
 with open('config.yml', 'r') as file:
     config = yaml.safe_load(file)
 
+
+logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.INFO)
+
 bot_server = config['bot']['server']
 bot_username = config['bot']['username']
-bot_access_token = config['bot']['access_token']
+try:
+    bot_access_token = config['bot']['access_token']
+    creds = botlib.Creds(bot_server,
+                         username=bot_username,
+                         access_token=bot_access_token)
+except KeyError:
+    logging.info("Using password based authentication for the bot")
+    try:
+        bot_access_password = config['bot']['password']
+    except KeyError:
+        error = "No access token or password for the bot provided"
+        logging.error(error)
+        raise KeyError(error)
+    creds = botlib.Creds(bot_server,
+                         username=bot_username,
+                         password=bot_access_password)
 api_base_url = config['api']['base_url']
 api_endpoint = config['api']['endpoint']
 api_token = config['api']['token']
-
-creds = botlib.Creds(bot_server,
-                     username=bot_username,
-                     access_token=bot_access_token)
 
 # Load a config file that configures bot behaviour
 config = botlib.Config()
 SIMPLE_MATRIX_BOT_CONFIG_FILE = "config.toml"
 try:
     config.load_toml(SIMPLE_MATRIX_BOT_CONFIG_FILE)
+    logging.info(f"Loaded the simple-matrix-bot config file {SIMPLE_MATRIX_BOT_CONFIG_FILE}")
 except FileNotFoundError:
+    logging.info(f"No simple-matrix-bot config file found. Creating {SIMPLE_MATRIX_BOT_CONFIG_FILE}")
     config.save_toml(SIMPLE_MATRIX_BOT_CONFIG_FILE)
 
 bot = botlib.Bot(creds, config)
@@ -69,10 +86,12 @@ async def token_actions(room, message):
 
         if match.command("create"):
             token = await api.create_token()
+            logging.info(f"User {match.event.sender} created token {token}")
             await bot.api.send_markdown_message(room.room_id, f"{RegistrationAPI.token_to_markdown(token)}")
 
         if match.command("delete-all"):
             deleted_tokens = await api.delete_all_token()
+            logging.info(f"User {match.event.sender} deleted all token")
             await send_info_on_deleted_token(room, deleted_tokens)
 
         if match.command("delete"):
@@ -85,6 +104,7 @@ async def token_actions(room, message):
                     await bot.api.send_text_message(room.room_id, f"Error: {e.args[0]}")
                 except FileNotFoundError as e:
                     await bot.api.send_text_message(room.room_id, f"Error: {e.args[0]}")
+            logging.info(f"User {match.event.sender} deleted token {deleted_tokens}")
             await send_info_on_deleted_token(room, deleted_tokens)
 
         if match.command("show"):
@@ -103,12 +123,14 @@ async def token_actions(room, message):
         if match.command("allow"):
             bot.config.add_allowlist(set(match.args()))
             bot.config.save_toml("config.toml")
+            logging.info(f"User {match.event.sender} allowed {set(match.args())}")
             await bot.api.send_text_message(
                 room.room_id,
                 f'allowing {", ".join(arg for arg in match.args())}')
 
         if match.command("disallow"):
             bot.config.remove_allowlist(set(match.args()))
+            logging.info(f"User {match.event.sender} disallowed {set(match.args())}")
             await bot.api.send_text_message(
                 room.room_id,
                 f'disallowing {", ".join(arg for arg in match.args())}')
