46d9182378a4e781e1c4dcdbf08567598775dea8
Julian-Samuel Gebühr <julian-samuel@gebuehr.net>
2022-02-22 22:53:18 +0100

    Bump version to v1.1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="matrix-registration-bot",
-    version="1.0.0",
+    version="1.1.0",
     description="A bot to manage user registration tokens on a matrix server.",
     long_description=long_description,
     long_description_content_type="text/markdown",
